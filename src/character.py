bc81de1345ea6eb9a0d7313aac1bf0f1943196ad
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-10-14 08:29:09 +0200

    Character position update into method.

--- src/character.py
+++ src/character.py
@@ -1,4 +1,5 @@
 import constants as c
+import pygame
 
 class Character:
     x_a = c.g
@@ -38,3 +39,42 @@ class Character:
         """
         return (block.y_pos - self.y_size < self.y_pos < block.y_pos + block.y_size
         and block.x_pos + block.x_size <= self.x_pos)
+
+    def update_pos(self, block, keys, t):
+        # Acceleration
+        self.y_a *= 0.9 # Energy loss
+
+        if keys[pygame.K_SPACE] and self.x_pos == c.X_GROUND:
+            t['t'] = 1
+            self.x_v0 = -20
+
+        if keys[pygame.K_a]:
+            self.y_a = -2
+
+        if keys[pygame.K_d]:
+            self.y_a = 2
+
+        # Position
+        x_old_pos = self.x_pos
+        y_old_pos = self.y_pos
+
+        if self.x_pos <= c.X_GROUND:
+            self.x_pos = 0.5 * self.x_a * t['t'] ** 2 + self.x_v0 * t['t'] + self.x_pos0
+            t['t'] += 0.20
+        if self.x_pos > c.X_GROUND:
+            self.x_pos = c.X_GROUND
+
+        if self.y_a < 0 and self.y_pos > 0:
+            self.y_pos += self.y_a * 1
+
+        if self.y_a > 0 and self.y_pos < c.SURFWIDTH - c.TILESIZE:
+            self.y_pos += self.y_a * 1
+
+        if self.collides(block):
+            if self.is_above(block): # Can't go down
+                self.x_pos = block.x_pos - self.x_size
+                self.is_on_block = True
+            elif self.is_under(block): # Can't go up
+                self.x_pos = x_old_pos
+            else:
+                self.y_pos = y_old_pos
--- src/pyrena.py
+++ src/pyrena.py
@@ -16,7 +16,7 @@ def main():
     char = Character()
     block = Block()
 
-    t = 0
+    t = {'t': 0} # Dict are mutable, allowing to pass arguments by reference
 
     while True:
         # EVENT
@@ -25,46 +25,8 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        # LOSING ENERGY
-        char.y_a *= 0.9
-
-        # UPDATE ACCELERATION
         keys = pygame.key.get_pressed()
-
-        if keys[K_SPACE] and char.x_pos == c.X_GROUND:
-            t = 1
-            char.x_v0 = -20
-
-        if keys[K_a]:
-            char.y_a = - 2
-
-        if keys[K_d]:
-            char.y_a = 2
-
-        # UPDATE POS
-        x_old_pos = char.x_pos
-        y_old_pos = char.y_pos
-
-        if char.x_pos <= c.X_GROUND:
-            char.x_pos = 0.5 * char.x_a * t ** 2 + char.x_v0 * t + char.x_pos0
-            t += 0.20
-        if char.x_pos > c.X_GROUND:
-            char.x_pos = c.X_GROUND
-
-        if char.y_a < 0 and char.y_pos > 0:
-            char.y_pos += char.y_a * 1
-
-        if char.y_a > 0 and char.y_pos < c.SURFWIDTH - c.TILESIZE:
-            char.y_pos += char.y_a * 1
-
-        if char.collides(block):
-            if char.is_above(block): # Can't go down
-                char.x_pos = block.x_pos - char.x_size
-                char.is_on_block = True
-            elif char.is_under(block): # Can't go up
-                char.x_pos = x_old_pos
-            else:
-                char.y_pos = y_old_pos
+        char.update_pos(block, keys, t)
 
         ### DRAW ###
         for x in range(c.MAPHEIGHT):
